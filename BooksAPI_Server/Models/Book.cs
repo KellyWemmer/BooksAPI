2012da4a938ccf73787679bb3bab686fb7c3a371
Kelly Wemmer <37150725+KellyWemmer@users.noreply.github.com>
2022-11-03 14:37:45 -0600

    test api call for author_name in console

--- BooksAPI_Server/Models/Book.cs
+++ BooksAPI_Server/Models/Book.cs
@@ -1,14 +1,19 @@
-﻿namespace BooksAPI_Server.Models
+﻿using Newtonsoft.Json;
+
+namespace BooksAPI_Server.Models
 {
     public class Book
     {
         public string? Id { get; set; }
         public string? title { get; set; }
-        public int first_publish_year { get; set; }
-        List<string>? isbn { get; set; }
-        public string? author_name { get; set; }
-        public int cover_i { get; set; }
-        public string? language { get; set; }
-        List<string>? subject { get; set; }
+        [JsonProperty("first_publish_year")]
+        public int firstPublishYear { get; set; }
+        public List<string>? isbn { get; set; }
+        [JsonProperty("author_name")]
+        public List<string> authorName { get; set; }
+        [JsonProperty("cover_i")]
+        public int coverI { get; set; }
+        public List<string>? language { get; set; }
+        public List<string>? subject { get; set; }
     }
 }
--- BooksAPI_Server/Program.cs
+++ BooksAPI_Server/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+//builder.Services.AddMvc().AddNewtonsoftJson();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpClient(); //Similar to axios client
 
