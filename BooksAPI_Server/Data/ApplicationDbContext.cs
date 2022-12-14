43d19d0a83b869878f5410cbaa6672304f26ce17
Kelly Wemmer <37150725+KellyWemmer@users.noreply.github.com>
2022-12-14 10:08:34 -0700

    Created models for SQL Server and DbContext

---
+++ BooksAPI_Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BooksAPI_Server.Models;
+
+namespace BooksAPI_Server.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public DbSet<Favorite> Favorite { get; set; }
+        public DbSet<ToRead> ToRead { get; set; }
+        public DbSet<Finished> Finished { get; set; }    
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+    }
+}
---
+++ BooksAPI_Server/Models/Favorite.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksAPI_Server.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public string Id { get; set; }
+        public string key { get; set; }
+        public string title { get; set; }
+
+        public List<string> isbn { get; set; }
+
+        public List<string> author_name { get; set; }
+    }
+}
