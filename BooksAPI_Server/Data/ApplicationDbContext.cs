20723f06c330012ae97d85e8dd1645fde0a2d09a
Kelly Wemmer <37150725+KellyWemmer@users.noreply.github.com>
2022-12-15 12:46:15 -0700

    Added author tables to DbContext

--- BooksAPI_Server/Data/ApplicationDbContext.cs
+++ BooksAPI_Server/Data/ApplicationDbContext.cs
@@ -6,8 +6,11 @@ namespace BooksAPI_Server.Data
     public class ApplicationDbContext : DbContext 
     {
         public DbSet<Favorite> Favorite { get; set; }
+        public DbSet<FavoriteAuthor> FavoriteAuthor { get; set; }
         public DbSet<ToRead> ToRead { get; set; }
-        public DbSet<Finished> Finished { get; set; }    
+        public DbSet<ToReadAuthor> ToReadAuthor { get; set; }
+        public DbSet<Finished> Finished { get; set; }   
+        public DbSet<FinishedAuthor> FinishedAuthor { get; set; }
 
         public DbSet<Author> Author { get; set; }
         
