f9cae4a407e71c290509932a954722a48e4ab285
Kelly Wemmer <37150725+KellyWemmer@users.noreply.github.com>
2022-12-19 10:34:10 -0700

    Build ToRead repo

--- BooksAPI_Server/Repositories/ToReadRepository.cs
+++ BooksAPI_Server/Repositories/ToReadRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BooksAPI_Server.Data;
 using BooksAPI_Server.Models;
+using Microsoft.Win32.SafeHandles;
 
 namespace BooksAPI_Server.Repositories
 {
@@ -12,6 +13,36 @@ namespace BooksAPI_Server.Repositories
             this._context = context;
         }
 
-       
+        internal List<ToRead> GetToReads() 
+        {
+            return _context.ToReads.ToList();
+        }
+
+        internal ToRead CreateToRead(ToRead newToRead) 
+        {
+            _context.ToReads.Add(newToRead);
+            _context.SaveChanges();
+            return newToRead;
+        }
+
+        internal ToRead GetToReadById(int id) 
+        {
+            ToRead toRead = _context.ToReads.FirstOrDefault(r => r.Id == id);
+            return toRead;
+        }
+
+        internal void DeleteToRead(int id) 
+        {
+            ToRead toRead = GetToReadById(id);
+            if(toRead != null)
+            {
+                _context.ToReads.Remove(toRead);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Book ToRead not found");
+            }
+        }
     }
 }
