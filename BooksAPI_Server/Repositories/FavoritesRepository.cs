02ca5a8ffa2c248b342e398137d6ca3606158e39
Kelly <kellysue3@yahoo.com>
2023-01-24 13:02:11 -0700

    Add To Favorites method

--- BooksAPI_Server/Repositories/FavoritesRepository.cs
+++ BooksAPI_Server/Repositories/FavoritesRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace BooksAPI_Server.Repositories
+﻿using BooksAPI_Server.Data;
+using BooksAPI_Server.Models;
+
+namespace BooksAPI_Server.Repositories
 {
     public class FavoritesRepository
     {
+        private readonly ApplicationDbContext _context;
+        public FavoritesRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        internal Favorite CreateFavorite(Favorite newFavorite)
+        {
+            _context.Favorites.Add(newFavorite);   
+            _context.SaveChanges();
+            return newFavorite;
+        }
     }
 }
